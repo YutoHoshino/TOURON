eb4f046aa27e98fd03517ec23f945dcb5541f2c0
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-06-04 20:03:44 +0900

    DB_update
    マイグレーションファイルの細かい記載を修正

--- db/migrate/20200604105225_create_talks.rb
+++ db/migrate/20200604105225_create_talks.rb
@@ -1,10 +1,10 @@
--class CreateTalks < ActiveRecord::Migration[5.2]
+class CreateTalks < ActiveRecord::Migration[5.2]
   def change
     create_table :talks do |t|
       t.text :text, null:false
       t.string :image
       # t.references :user, foreign_key: true
-      t.references :room, foreign_key: true
+      # t.references :room, foreign_key: true
       t.integer :status_id
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_054732) do
+ActiveRecord::Schema.define(version: 2020_06_04_105444) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_054732) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-ActiveRecord::Schema.define(version: 2020_06_04_054600) do
 
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -30,9 +29,9 @@ ActiveRecord::Schema.define(version: 2020_06_04_054600) do
   create_table "talks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
     t.string "image"
+    t.integer "status_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "talks", "rooms"
 end
