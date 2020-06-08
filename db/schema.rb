9f8bebead3683ba6fd1d9faee1bec9da281241b6
yutasasao <ss.yuta@icloud.com>
2020-06-08 09:11:59 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
- 
 ActiveRecord::Schema.define(version: 2020_06_04_105225) do
 
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,8 +25,8 @@ ActiveRecord::Schema.define(version: 2020_06_04_105225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
-   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -38,5 +36,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_105225) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
