244c05a25d1d7d452d4b6205735add3bb63842a1
Naoto3615 <supra3615@gmail.com>
2020-06-09 11:07:36 +0900

    update homes_header

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_054732) do
+ActiveRecord::Schema.define(version: 2020_06_08_014844) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
     t.string "name", null: false
     t.text "description"
+    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -34,18 +35,20 @@ ActiveRecord::Schema.define(version: 2020_06_04_054732) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-  add_foreign_key "talks", "rooms"
-=======
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", null: false
+    t.text "profile"
+    t.string "image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
->>>>>>> 7354b80dbf33af97718b1e8c4415279952795b43
 end
