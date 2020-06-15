8bb1cd24c5cd388ea5683a01693acaed5e8c3cb1
yutasasao <ss.yuta@icloud.com>
2020-06-15 16:04:53 +0900

    tag search
    ##what
    タグ検索機能、及びタグ検索結果表示作成。

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,12 @@
 class RoomsController < ApplicationController
 
+  def index
+    #タグ絞り込み
+    if params[:tag_name]
+      @rooms = Room.tagged_with("#{params[:tag_name]}")
+    end
+  end
+
   def new
     @rooms = Room.new
   end
@@ -12,7 +19,7 @@ class RoomsController < ApplicationController
 
   private
   def room_params
-    params.require(:room).permit(:image, :name, :description, :category_id, :period, :tag)
+    params.require(:room).permit(:image, :name, :description, :category_id, :period, :tag_list)
   end
 
 end
