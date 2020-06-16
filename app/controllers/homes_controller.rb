6da02b4244c754731f578d45cee4caaa8a81a432
yutasasao <ss.yuta@icloud.com>
2020-06-16 17:41:46 +0900

    room-random
    ##what
    ・全てのroomからランダムで表示
    ・ページ更新するたびに再度ランダム表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,8 @@ class HomesController < ApplicationController
 
   def index
     @rooms = Room.all.order(id: "DESC").first(10)
+    @room = Room.all
+    @room_random = @room.sample(9) #ランダムで９つ取得
     @category = Category.all
   end 
 
