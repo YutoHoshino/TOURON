c03a5bf34557b429098ae28463063d73bdcc81c6
YutoHoshino <hoshinoyuto0518@gmail.com>
2020-06-24 18:41:54 +0900

    home-sever
    ホームのデザイン変更サーバーサイド
    ・jsで横のユーザー画面を表示非表示
    ・navbarの変更

---
+++ app/assets/javascripts/homes/side_var.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', ()=>  {
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1000 && $(this).scrollTop() <= 3500) {
+      console.log('hello');
+      $('.user-side-box-area').addClass("fixed");
+    } else {
+      $('.user-side-box-area').removeClass("fixed");
+    }
+
+
+  });
+});
+
