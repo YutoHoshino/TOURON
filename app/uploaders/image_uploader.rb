75af7ffc8f5ad8afe3d4612d8fdca7f6e4184dc0
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-07-01 12:06:26 +0900

    s3_edit
    s3へのアップロードが行えるように更新

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -43,4 +43,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
+  
 end
