a861b062363f42a38ae2b5d3b71fe3859dca6cf9
mochi2000 <umaibou2000jp@yahoo.co.jp>
2020-07-01 16:33:38 +0900

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery 
-//= require jquery_ujs 
 //= require rails-ujs
 //= require activestorage
+//= require jquery 
 //= require turbolinks
 //= require_tree .
